e75afd30913173c902252251db79c7c17e1f7d2b
Kato Akiru <33294906+paithiov909@users.noreply.github.com>
2022-03-24 06:13:54 +0900

    fix: discard EOS token

--- R/pack.R
+++ R/pack.R
@@ -4,5 +4,7 @@
 #' @inheritParams audubon::pack
 #' @export
 pack <- function(df, n = 1L, pull = "token", sep = "-", .collapse = " ") {
-  audubon::pack(df, n, pull = pull, sep = sep, .collapse = .collapse)
+  df %>%
+    dplyr::filter(.data$token != "EOS") %>%
+    audubon::pack(n, pull = pull, sep = sep, .collapse = .collapse)
 }
