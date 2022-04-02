8d55c51f08cdd05f3d6679b2f4b801cd4b5a6258
paithiov909 <am11.44@gmail.com>
2022-04-02 22:55:06 +0900

    refactor: update pack function

--- R/pack.R
+++ R/pack.R
@@ -3,8 +3,9 @@
 #' @inherit audubon::pack description return details sections seealso
 #' @inheritParams audubon::pack
 #' @export
-pack <- function(df, n = 1L, pull = "token", sep = "-", .collapse = " ") {
-  df %>%
+pack <- function(tbl, pull = "token", n = 1L, sep = "-", .collapse = " ") {
+  pull <- rlang::ensym(pull)
+  tbl %>%
     dplyr::filter(.data$token != "EOS") %>%
-    audubon::pack(n, pull = pull, sep = sep, .collapse = .collapse)
+    audubon::pack(pull, n = n, sep = sep, .collapse = .collapse)
 }
